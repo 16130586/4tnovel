2333b7da9f070e31f38fa2bc89b27cb573a69f49
NgocThat <ctckhjl>
2018-11-10 22:33:04 +0700

    basic born for register feature, on process 50%

--- src/t4novel/azurewebsites/net/servlets/RegisterServlet.java
+++ src/t4novel/azurewebsites/net/servlets/RegisterServlet.java
@@ -1,11 +1,18 @@
 package t4novel.azurewebsites.net.servlets;
 
 import java.io.IOException;
+import java.util.Map.Entry;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import t4novel.azurewebsites.net.acessviasocial.DAOSUtils.DAOUtils;
+import t4novel.azurewebsites.net.acessviasocial.DAOService.EmailCheckingService;
+import t4novel.azurewebsites.net.acessviasocial.DAOService.UserNameCheckingService;
+import t4novel.azurewebsites.net.forms.RegisterForm;
 
 /**
  * Servlet implementation class RegisterServlet
@@ -13,38 +20,46 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public RegisterServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		getServletContext().getRequestDispatcher("/jsps/pages/register.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getParameterMap();
-		String email = request.getParameter("email");
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		String rePassword = request.getParameter("re-password");
-		String agreement = request.getParameter("agreement");
-		System.out.println("username: " + username);
-		System.out.println("password: " + password);
-		System.out.println("re-password: " + rePassword);
-		System.out.println("email: " + email);
-		System.out.println("agree: " + agreement);
-		getServletContext().getRequestDispatcher("/jsps/pages/register.jsp").forward(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		DAOUtils daoUtil = DAOUtils.getInstance();
+		RegisterForm userSubmittedForm = new RegisterForm(request, new EmailCheckingService(daoUtil),
+				new UserNameCheckingService(daoUtil));
+		String url = "";
+		if (!userSubmittedForm.isOnError()) {
+			url = "/index";
+			// TODO write account to DTB
+			// TODO set account to ss
+		} else {
+			url = "/jsps/pages/register.jsp";
+			for (Entry<String, String> errors : userSubmittedForm.getErrors().entrySet()) {
+				String errorName = errors.getKey() + "Error";
+				request.setAttribute(errorName, errors.getValue());
+			}
+		}
+		getServletContext().getRequestDispatcher(url).forward(request, response);
 	}
 }
