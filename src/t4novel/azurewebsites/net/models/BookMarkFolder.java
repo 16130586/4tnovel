5e3ac55bbfd715cd6ff4be84aeb1f234e5bb4b60
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2018-11-24 19:43:52 +0700

    adding ownder id attribute

--- src/t4novel/azurewebsites/net/models/BookMarkFolder.java
+++ src/t4novel/azurewebsites/net/models/BookMarkFolder.java
@@ -3,7 +3,7 @@ package t4novel.azurewebsites.net.models;
 import java.util.List;
 
 public class BookMarkFolder {
-	private int id;
+	private int id, accountOnwerID;
 	private List<BookMark> bookMarks;
 	private String title;
 
@@ -11,11 +11,12 @@ public class BookMarkFolder {
 		
 	}
 	
-	public BookMarkFolder(int id, List<BookMark> bookMarks, String title) {
+	public BookMarkFolder(int id, int accountOnwerId, List<BookMark> bookMarks, String title) {
 		super();
 		this.id = id;
 		this.bookMarks = bookMarks;
 		this.title = title;
+		this.accountOnwerID = accountOnwerId;
 	}
 
 	public int getId() {
@@ -24,6 +25,14 @@ public class BookMarkFolder {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public int getAccountOnwerID() {
+		return accountOnwerID;
+	}
+
+	public void setAccountOnwerID(int accountOnwerID) {
+		this.accountOnwerID = accountOnwerID;
 	}
 
 	public List<BookMark> getBookMarks() {
