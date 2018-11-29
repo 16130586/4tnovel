ef0f2ad555162db4ab7f4bf20194aa3e9d28bb74
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2018-11-29 08:40:44 +0700

    adding finally closing statement and resultSet

--- src/t4novel/azurewebsites/net/DAOService/ExistedPasswordCheckingService.java
+++ src/t4novel/azurewebsites/net/DAOService/ExistedPasswordCheckingService.java
@@ -6,24 +6,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ExistedPasswordCheckingService extends BaseDaoService implements DAOService {
-	private Connection cnn;
 
 	public ExistedPasswordCheckingService(Connection databaseConnection) {
-		this.cnn = databaseConnection;
+		super(databaseConnection);
 	}
 
 	@Override
 	public boolean check(String data1, String data2, String onQuery) {
-		PreparedStatement stmt;
+		PreparedStatement stmt = null;
 		boolean isExisted = false;
+		ResultSet rs = null;
 		try {
 			stmt = cnn.prepareStatement(onQuery);
 			stmt.setInt(1, Integer.parseInt(data1));
 			stmt.setString(2, data2);
-			ResultSet rs = stmt.executeQuery();
+			rs = stmt.executeQuery();
 			isExisted = rs.next();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 		return isExisted;
