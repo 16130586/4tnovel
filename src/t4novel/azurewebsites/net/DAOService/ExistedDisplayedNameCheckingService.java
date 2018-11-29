d02e6c2cf5524f34f09206b53a8d9483c9d565b7
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2018-11-29 08:36:17 +0700

    adding finally closing statement and resultSet in DAOServices

--- src/t4novel/azurewebsites/net/DAOService/ExistedDisplayedNameCheckingService.java
+++ src/t4novel/azurewebsites/net/DAOService/ExistedDisplayedNameCheckingService.java
@@ -3,25 +3,35 @@ package t4novel.azurewebsites.net.DAOService;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ExistedDisplayedNameCheckingService extends BaseDaoService implements DAOService {
-	private Connection cnn;
-	
+
 	public ExistedDisplayedNameCheckingService(Connection databaseConnection) {
-		this.cnn = databaseConnection;
+		super(databaseConnection);
 	}
-	
+
 	@Override
 	public boolean check(String data, String onQuery) {
-		PreparedStatement sttm;
+		PreparedStatement stm = null;
 		boolean isExisted = false;
+		ResultSet rs = null;
 		try {
-			sttm = cnn.prepareStatement(onQuery);
-			sttm.setString(1, data);
-			ResultSet rs = sttm.executeQuery();
+			stm = cnn.prepareStatement(onQuery);
+			stm.setString(1, data);
+			rs = stm.executeQuery();
 			isExisted = rs.next();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stm != null)
+					stm.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return isExisted;
 	}
--- src/t4novel/azurewebsites/net/DAOService/LoginCheckingService.java
+++ src/t4novel/azurewebsites/net/DAOService/LoginCheckingService.java
@@ -5,28 +5,37 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LoginCheckingService extends BaseDaoService implements DAOService{
-	private Connection cnn;
+public class LoginCheckingService extends BaseDaoService implements DAOService {
 
 	public LoginCheckingService(Connection databaseConnection) {
-		this.cnn = databaseConnection;
+		super(databaseConnection);
 	}
 
 	@Override
-	public boolean check(String data1,String data2, String onQuery) {
-		PreparedStatement stmt;
+	public boolean check(String data1, String data2, String onQuery) {
+		PreparedStatement stmt = null;
 		boolean isExisted = false;
+		ResultSet rs = null;
 		try {
 			stmt = cnn.prepareStatement(onQuery);
 			stmt.setString(1, data1);
 			stmt.setString(2, data2);
-			ResultSet rs = stmt.executeQuery();
+			rs = stmt.executeQuery();
 			isExisted = rs.next();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
+		}
 		return isExisted;
 	}
-	
+
 }
