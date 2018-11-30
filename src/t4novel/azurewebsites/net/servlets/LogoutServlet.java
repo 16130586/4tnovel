ffbc457e471a7ec52604d72b4266eba2be5393f1
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2018-11-30 16:36:14 +0700

    fix session stuck logic

--- src/t4novel/azurewebsites/net/servlets/LogoutServlet.java
+++ src/t4novel/azurewebsites/net/servlets/LogoutServlet.java
@@ -1,11 +1,14 @@
 package t4novel.azurewebsites.net.servlets;
 
 import java.io.IOException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
@@ -20,6 +23,13 @@ public class LogoutServlet extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		HttpSession session = request.getSession();
+//		Enumeration<String> attributeInSessionNames = session.getAttributeNames();
+//		while(attributeInSessionNames.hasMoreElements()) {
+//			String attName = attributeInSessionNames.nextElement();
+//			session.setAttribute(attName, null);
+//		}
+//		session.invalidate();
 		request.getSession().invalidate();
 		response.sendRedirect("index");
 	}
