493c7501d4c5840d9afce887ddbeb14e711da69a
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2018-11-30 14:16:13 +0700

    adding doPost and doGet logic for AddingVolServlet - controller

--- src/t4novel/azurewebsites/net/servlets/AddingVolServlet.java
+++ src/t4novel/azurewebsites/net/servlets/AddingVolServlet.java
@@ -2,6 +2,9 @@ package t4novel.azurewebsites.net.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import t4novel.azurewebsites.net.DAO.NovelDAO;
 import t4novel.azurewebsites.net.DAO.VolDAO;
 import t4novel.azurewebsites.net.forms.AbstractMappingForm;
 import t4novel.azurewebsites.net.forms.AddingVolForm;
+import t4novel.azurewebsites.net.models.Account;
 import t4novel.azurewebsites.net.models.Vol;
 
 /**
@@ -20,42 +25,68 @@ import t4novel.azurewebsites.net.models.Vol;
 @WebServlet("/add-vol")
 public class AddingVolServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddingVolServlet() {
-        super();
-      
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public AddingVolServlet() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Account hostAccount = (Account) request.getSession().getAttribute("account");
+		if (hostAccount.getOwnNovels() == null)
+			try {
+				Connection cnn = (Connection) request.getAttribute("connection");
+				NovelDAO novelDao = new NovelDAO(cnn);
+				hostAccount.setOwnNovels(novelDao.getNovelsByUserId(hostAccount.getId()));
+			} catch (Exception e) {
+				e.printStackTrace();
+				response.sendError(500);
+			}
 		getServletContext().getRequestDispatcher("/jsps/pages/add-vol.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+
 		AbstractMappingForm form = new AddingVolForm(request);
-		if(!form.isOnError()) {
-			//TODO writing to db , and something related
+		if (!form.isOnError()) {
+			// TODO writing to db , and something related
 			Connection cnn = (Connection) request.getAttribute("connection");
+			
 			VolDAO VolDAO = new VolDAO(cnn);
 			Vol vol = (Vol) form.getMappingData();
+			try {
+				cnn.setAutoCommit(false);
+				VolDAO.insertVol(vol);
+				request.setAttribute("sucessed", "Adding new chapter done!");
+				cnn.setAutoCommit(true);
+			} catch ( Exception e) {
+				try {
+					cnn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				e.printStackTrace();
+			}
 			
-			VolDAO.insertVol(vol);
-			// set sucessed for user
-			request.setAttribute("sucessed", "Adding new chapter done!");
-			System.out.println("suceess!");
-		}
-		else {
+			
+		} else {
 			form.applyErrorsToUI(request);
-			System.out.println("eror!");
 		}
 		getServletContext().getRequestDispatcher("/jsps/pages/add-vol.jsp").forward(request, response);
 	}
