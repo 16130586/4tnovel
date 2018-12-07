54779cd57d5275b8165aa8bce0bd63daaeb4c43c
NgocThat <ctckhjl>
2018-12-07 15:37:19 +0700

    adding loading 5 newest message to inbox and adding user to userOnline pool

--- src/t4novel/azurewebsites/net/servlets/LoginServlet.java
+++ src/t4novel/azurewebsites/net/servlets/LoginServlet.java
@@ -8,15 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import t4novel.azurewebsites.net.DAO.AccountDAO;
 import t4novel.azurewebsites.net.DAO.BookmarkFolderDAO;
 import t4novel.azurewebsites.net.DAO.FollowDAO;
+import t4novel.azurewebsites.net.DAO.InboxDAO;
 import t4novel.azurewebsites.net.DAOService.DAOService;
 import t4novel.azurewebsites.net.DAOService.LoginCheckingService;
 import t4novel.azurewebsites.net.forms.AbstractMappingForm;
 import t4novel.azurewebsites.net.forms.LoginForm;
 import t4novel.azurewebsites.net.models.Account;
+import t4novel.azurewebsites.net.session.OnlineAccounts;
 
 /**
  * Servlet implementation class LoginServlet
@@ -51,6 +54,7 @@ public class LoginServlet extends HttpServlet {
 		DAOService loginCheckingService = new LoginCheckingService(cnn);
 		AccountDAO accountDAO = new AccountDAO(cnn);
 		FollowDAO followDao = new FollowDAO(cnn);
+		InboxDAO inboxDao = new InboxDAO(cnn);
 		BookmarkFolderDAO bookmarkFolderDAO = new BookmarkFolderDAO(cnn);
 		AbstractMappingForm loginForm = new LoginForm(request, loginCheckingService);
 
@@ -61,12 +65,15 @@ public class LoginServlet extends HttpServlet {
 				account = accountDAO.getAccountByUsername(account.getUserName());
 				account.setBookMarkFolders(bookmarkFolderDAO.getBookmarkFolderByUser(account.getId()));
 				account.setNovelFollowsId(followDao.getNovelFollow(account.getId()));
+				account.setMessages(inboxDao.getMessagesInInBox(0 , 5 , account.getId()));
 				//TODO load vua du ra man hinh bao hom username, bla bla, bookmarks, thong bao
 				//TODO load them thong bao len nua
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			request.getSession().setAttribute("account", account);
+			HttpSession session = request.getSession();
+			session.setAttribute("account", account);
+			OnlineAccounts.onLoginAccount(account);
 			response.sendRedirect("index");
 		} else {
 			loginForm.applyErrorsToUI(request);
