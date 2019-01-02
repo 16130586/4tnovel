644c7c73efc25d1b6c0fc05ca78cbd0f75b782cf
aspiration2017 <nhoktuyenpro@gmail.com>
2019-01-02 21:25:04 +0700

    load trendingNovel, top month novel to index

--- src/t4novel/azurewebsites/net/DAO/ViewDAO.java
+++ src/t4novel/azurewebsites/net/DAO/ViewDAO.java
@@ -2,7 +2,10 @@ package t4novel.azurewebsites.net.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ViewDAO {
 	private Connection cnn;
@@ -22,7 +25,21 @@ public class ViewDAO {
 		stmt.close();
 	}
 
-
+	public List<Integer> getTopViewNovelsId(int day, int offset, int limit) throws SQLException {
+		List<Integer> result = new LinkedList<>();
+		String query = "select target_id, count(target_id) as [view] from viewing where stream ='novel' and viewdate > DATEADD(DAY, -?, getdate() AT TIME ZONE 'SE Asia Standard Time') group by target_id order by [view] desc offset ? rows fetch next ? rows only";
+		PreparedStatement stmt = cnn.prepareStatement(query);
+		stmt.setInt(1, day);
+		stmt.setInt(2, offset);
+		stmt.setInt(3, limit);
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			result.add(rs.getInt("TARGET_ID"));
+		}
+		rs.close();
+		stmt.close();
+		return result;
+	}
 	
 
 }
