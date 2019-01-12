0964bb951910cd4b91d695f6cb8458311bcaed9b
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2019-01-12 21:24:38 +0700

    init login via google

--- src/t4novel/azurewebsites/net/acessviasocial/common/SocialAccessConstants.java
+++ src/t4novel/azurewebsites/net/acessviasocial/common/SocialAccessConstants.java
@@ -1,9 +1,9 @@
 package t4novel.azurewebsites.net.acessviasocial.common;
 
 public class SocialAccessConstants {
-	public static final String GOOGLE_CLIENT_ID = "721875041178-qlgu0ftc55jini0r9ul1rarbquq09bvu.apps.googleusercontent.com";
-	public static final String GOOGLE_CLIENT_SERECT = "kKA8pyY3P249L8tslwg7vbAH";
-	public static final String GOOGLE_REDIRECT_URI = "http://localhost:6560/net.azurewebsites.t4novel/login-google";
+	public static final String GOOGLE_CLIENT_ID = "901230047480-kgieqoea1ee4qi0kkb1ins51al2pko2t.apps.googleusercontent.com";
+	public static final String GOOGLE_CLIENT_SERECT = "9zuD9qIIB26OLvsTl8TCHBvA";
+	public static final String GOOGLE_REDIRECT_URI = "https://tieuthuyetonline.azurewebsites.net/login-google";
 	public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 	public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
 	public static final String GOOGLE_GRANT_TYPE = "authorization_code";
--- src/t4novel/azurewebsites/net/servlets/LoginViaGoogle.java
+++ src/t4novel/azurewebsites/net/servlets/LoginViaGoogle.java
@@ -1,15 +1,33 @@
 package t4novel.azurewebsites.net.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.restfb.types.User;
+
+import t4novel.azurewebsites.net.DAO.AccountDAO;
+import t4novel.azurewebsites.net.DAO.BookMarkDAO;
+import t4novel.azurewebsites.net.DAO.BookmarkFolderDAO;
+import t4novel.azurewebsites.net.DAO.FollowDAO;
+import t4novel.azurewebsites.net.DAO.InboxDAO;
+import t4novel.azurewebsites.net.DAOService.EmailCheckingService;
+import t4novel.azurewebsites.net.DAOService.UserNameCheckingService;
+import t4novel.azurewebsites.net.acessviafacebook.common.RestFB;
 import t4novel.azurewebsites.net.acessviagoogle.common.GooglePojo;
 import t4novel.azurewebsites.net.acessviagoogle.common.GoogleUtils;
+import t4novel.azurewebsites.net.forms.AbstractMappingForm;
+import t4novel.azurewebsites.net.forms.RegisterSocialForm;
+import t4novel.azurewebsites.net.models.Account;
+import t4novel.azurewebsites.net.models.BookMarkFolder;
+import t4novel.azurewebsites.net.session.OnlineAccounts;
 
 /**
  * Servlet implementation class LoginViaGoogle
@@ -37,19 +55,100 @@ public class LoginViaGoogle extends HttpServlet {
 	      } else {
 	        String accessToken = GoogleUtils.getToken(code);
 	        GooglePojo googlePojo = GoogleUtils.getUserInfo(accessToken);
-	        request.setAttribute("id", googlePojo.getId());
-	        request.setAttribute("name", googlePojo.getName());
-	        request.setAttribute("email", googlePojo.getEmail());
-	        url="/index";
+	        
+			Connection cnn = (Connection) request.getAttribute("connection");
+			
+			AccountDAO accountDAO = new AccountDAO(cnn);
+			FollowDAO followDao = new FollowDAO(cnn);
+			InboxDAO inboxDao = new InboxDAO(cnn);
+			BookmarkFolderDAO bookmarkFolderDAO = new BookmarkFolderDAO(cnn);
+			BookMarkDAO bookmarkDAO = new BookMarkDAO(cnn);
+			Account account = null;
+
+			try {
+				// check google id da co trong db chua?
+				account = accountDAO.getAccountByGoogleID(googlePojo.getId());
+				System.out.println(account);
+				boolean ggIdExisted = true;
+				// neu gg id khong co trong db thi check email xem
+				if (account == null) {
+					account = accountDAO.getAccountByEmail(googlePojo.getEmail());
+					ggIdExisted = false;
+				}
+				// neu gmail ton tai va gg id khong co -> them gg id vao
+				if (account != null && ggIdExisted == false) {
+					accountDAO.updateAccountGoogleID(account.getId(), googlePojo.getId());
+				}
+				// neu gmail khong ton tai thi tao moi account, dua ra form cho nguoi dung nhap
+				if (account == null && ggIdExisted == false) {
+					request.setAttribute("ggID", googlePojo.getId());
+					request.setAttribute("ggName", googlePojo.getName());
+					request.setAttribute("ggEmail", googlePojo.getEmail());
+					getServletContext().getRequestDispatcher("/jsps/pages/register-google.jsp").forward(request,
+							response);
+				} else {
+					// tien hanh dang nhap neu fb id co hoac gmail co ton tai trong db
+					List<BookMarkFolder> bookmarkFolder = bookmarkFolderDAO.getBookmarkFolderByUser(account.getId());
+					for (BookMarkFolder bmf : bookmarkFolder) {
+						bmf.setBookMarks(bookmarkDAO.getBookmarkByFolder(bmf.getId()));
+					}
+					account.setBookMarkFolders(bookmarkFolder);
+					account.setNovelFollowsId(followDao.getNovelFollow(account.getId()));
+					account.setMessages(inboxDao.getMessagesInInBox(0, 5, account.getId()));
+					HttpSession session = request.getSession();
+					session.setAttribute("account", account);
+					OnlineAccounts.onLoginAccount(account);
+					response.sendRedirect("index");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	      }
-		getServletContext().getRequestDispatcher(url).forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		Connection cnn = (Connection) request.getAttribute("connection");
+		AbstractMappingForm userSubmittedForm = new RegisterSocialForm(request, new EmailCheckingService(cnn),
+				new UserNameCheckingService(cnn));
 
+		String url = "";
+		if (!userSubmittedForm.isOnError()) {
+			AccountDAO accDAO = new AccountDAO(cnn);
+			url = "/index";
+			String ggID = request.getParameter("ggID");
+			Account account = (Account) userSubmittedForm.getMappingData();
+
+			try {
+				accDAO.insertAccount(account);
+				account.setId(accDAO.getNextID() - 1);
+				accDAO.updateAccountGoogleID(account.getId(), ggID);
+			} catch (Exception e) {
+				response.sendError(500);
+				e.printStackTrace();
+			}
+
+			request.getSession().setAttribute("account", account);
+			response.sendRedirect("index");
+		} else {
+			String ggName = request.getParameter("ggName");
+			String ggID = request.getParameter("ggID");
+			String ggEmail = request.getParameter("ggEmail");
+			
+			System.out.println(ggName);
+			
+			request.setAttribute("ggName", ggName);
+			request.setAttribute("ggID", ggID);
+			request.setAttribute("ggEmail", ggEmail);
+			
+			url = "/jsps/pages/register-google.jsp";
+			userSubmittedForm.applyErrorsToUI(request);
+			getServletContext().getRequestDispatcher(url).forward(request, response);
+		}
+	}
 }
